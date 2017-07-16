e01c5ca66d997d026da79313eedcbcee52d9bdd4
bredele <olivier.wietrich@gmail.com>
2017-07-15 20:46:52 -0600

    stream promise

--- test/salute.js
+++ test/salute.js
@@ -6,8 +6,9 @@ const test = require('tape')
 const server = require('server-test')
 const salute = require('..')
 const concat = require('concat-stream')
+const fs = require('fs')
 
-test('should stream string', assert => {
+test('should transfer string', assert => {
   assert.plan(1)
   server((req, res) => {
     salute(() => {
@@ -16,13 +17,22 @@ test('should stream string', assert => {
   })
 })
 
-test('should stream promise', assert => {
+test('should transfer promise', assert => {
   assert.plan(1)
   server((req, res) => {
     salute(() => {
       return new Promise((resolve, reject) => {
         resolve('hello world')
       })
+    })(req, res).pipe(concat(data => assert.equal(data.toString(), 'hello world')))
+  })
+})
+
+test('should transfer stream', assert => {
+  assert.plan(1)
+  server((req, res) => {
+    salute(() => {
+      return fs.createReadStream(__dirname + '/salute.txt')
     })(req, res).pipe(concat(data => assert.equal(data.toString(), 'hello world')))
   })
 })
