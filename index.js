0b3cda3517d96255620021b227af45b285bb8e7d
bredele <olivier.wietrich@gmail.com>
2017-07-17 16:04:30 -0600

    Resolve result into a promise to manage errors

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 const stream = require('morph-stream')
 const status = require('response-status')
 const lookup = require('mime-types').contentType
-const
-toString = Object.prototype.toString
+const toString = Object.prototype.toString
+
 
 /**
  * Create an HTTP request/response middlware which
@@ -21,11 +21,18 @@ toString = Object.prototype.toString
 module.exports = (middleware, type) => {
   return (req, res, ...args) => {
     const value = middleware(req, res, ...args)
-    res.setHeader('Content-Type', mime(value, type))
-    if (value instanceof Error) status(res, value.statusCode)
-    return stream(value)
+    return stream(Promise.resolve(value).then(val => {
+      if (val instanceof Error) {
+        status(res, val.statusCode)
+        return ''
+      } else {
+        res.setHeader('Content-Type', mime(val, type))
+        return val
+      }
+    }, reason => status(res, reason.statusCode)))
   }
 }
+
 
 
 /**
